342c34c2c5a444f90c7541f3eba339a8614b8832
minkowp <piotr.minkowski@play.pl>
2019-02-04 16:35:55 +0100

    fixing port number

--- passenger-management/src/test/java/pl/piomin/services/passenger/PassengerControllerComponentTests.java
+++ passenger-management/src/test/java/pl/piomin/services/passenger/PassengerControllerComponentTests.java
@@ -2,20 +2,25 @@ package pl.piomin.services.passenger;
 
 import io.specto.hoverfly.junit.core.HoverflyConfig;
 import io.specto.hoverfly.junit.rule.HoverflyRule;
+import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.Network;
+import org.testcontainers.containers.wait.strategy.Wait;
+import pl.piomin.services.passenger.model.Passenger;
 
 public class PassengerControllerComponentTests {
 
     private TestRestTemplate template = new TestRestTemplate();
 
     @ClassRule
-    public static GenericContainer app = new GenericContainer("piomin/passenger-management")
+    public static GenericContainer appContainer = new GenericContainer<>("piomin/passenger-management")
+            .withCreateContainerCmdModifier(cmd -> cmd.withName("passenger-management").withHostName("passenger-management"))
+            .withNetworkMode("tests")
+            .withNetworkAliases("passenger-management")
             .withExposedPorts(8080)
-            .withNetwork(Network.builder().id("tests").build());
+            .waitingFor(Wait.forHttp("/passengers"));
 
     @ClassRule
     public static HoverflyRule hoverflyRule = HoverflyRule
@@ -24,6 +29,8 @@ public class PassengerControllerComponentTests {
 
     @Test
     public void testFindByLogin() {
-
+        Passenger passenger = template.getForObject("http://passenger-management:8080/passengers/login/{login}", Passenger.class, "walker");
+        Assert.assertNotNull(passenger);
     }
+
 }
